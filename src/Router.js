c47c36b7c91861bf233efd648fe1d1a1498c4d30
lauren.madigan <lauren.madigan@workday.com>
2018-03-20 14:40:09 -0700

    router with new page for adding employee

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -10,9 +11,20 @@ const RouterComponent = () => {
       <Scene key="auth" >
         <Scene key="login" component={LoginForm} title="Please Login" initial />
         </Scene>
-      <Scene key="main">
-        <Scene key="employeelist" component={EmployeeList} title="Employees"  />
 
+      <Scene key="main">
+        <Scene
+          rightTitle="Add"
+          onRight={() => { Actions.employeecreate() }}
+          key="employeelist"
+          component={EmployeeList}
+          title="Employees"
+          initial />
+
+          <Scene
+            key="employeecreate"
+            component={EmployeeCreate}
+            title="Create Employee" />
       </Scene>
     </Scene>
     </Router>
